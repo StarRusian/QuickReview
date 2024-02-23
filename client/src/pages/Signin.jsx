81042b5aa3896ff26f59c4341a10b8f2d556e898
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-23 16:26:28 +0530

    complete sigin page with functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,113 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+// import OAuth from '../components/OAuth';
+import logo from "../assets/images/logo/newlogo.png";
+// import NavItems from '../components/NavItems';
+import AboutUs from '../home/AboutUs';
 
-export default function Signin() {
+
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ( !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
-    <div>Signin</div>
-  )
+    
+    <div className='min-h-screen mt-4 items-center justify-center '>
+      {/* <NavItems /> */}
+      <div className='flex p-2 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+        
+          {/* <p className='text-sm mt-5'>
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p> */}
+        </div>
+        {/* right */}
+
+        <div className='flex-1 mt-5 '>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            
+            <div>
+              <Label value='Email' />
+              <TextInput
+                type='email'
+                placeholder='@email.com'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+            {/* <OAuth /> */}
+          </form>
+          <div className='flex gap-2 text-sm mt-3'>
+            <span>Dont Have an account?</span>
+            <Link to='/signup' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+      <AboutUs />
+    </div>
+    
+  );
 }

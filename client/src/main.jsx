76cf57c27d7454b479047469031761e8df221d77
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-23 19:39:05 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/icofont.min.css";
 import "./assets/css/animate.css";
-import "./assets/css/style.min.css";  // Assuming this includes your Tailwind and Flowbite styles
+import "./assets/css/style.min.css"; // Assuming this includes your Tailwind and Flowbite styles
 
 // Import your components and application-specific styles
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import "./index.css";  // Assuming this includes more of your styles
+import "./index.css"; // Assuming this includes more of your styles
 import App from "./App.jsx";
 import Home from "./home/Home.jsx";
 import Shop from "./shop/Shop.jsx";
@@ -18,25 +18,27 @@ import SingleProduct from "./shop/SingleProduct.jsx";
 import About from "./about/About.jsx";
 import Signup from "./pages/Signup.jsx";
 import Signin from "./pages/Signin.jsx";
-import { store } from './redux/store.js';
-import { createRoot } from 'react-dom/client';
-
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { createRoot } from "react-dom/client";
 
 const Root = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="shop/:id" element={<SingleProduct />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="shop/:id" element={<SingleProduct />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </PersistGate>
 );
 
-createRoot(document.getElementById('root')).render(Root);
+createRoot(document.getElementById("root")).render(Root);

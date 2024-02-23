937ca1ac1ed4fa864d18b3cc997463c3f1e4f036
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-23 19:11:18 +0530

    add redux tool kit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,71 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-import "swiper/css";
-
-// bootstrap css
+// Import third-party libraries
+// import "swiper/css/swiper.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/css/icofont.min.css";
+import "./assets/css/animate.css";
+import "./assets/css/style.min.css";  // Assuming this includes your Tailwind and Flowbite styles
 
-// fonts and icons
-import "././assets/css/icofont.min.css";
-import "././assets/css/animate.css";
-import "././assets/css/style.min.css";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Import your components and application-specific styles
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";  // Assuming this includes more of your styles
+import App from "./App.jsx";
 import Home from "./home/Home.jsx";
-
 import Shop from "./shop/Shop.jsx";
 import SingleProduct from "./shop/SingleProduct.jsx";
 import About from "./about/About.jsx";
 import Signup from "./pages/Signup.jsx";
 import Signin from "./pages/Signin.jsx";
-// import AuthProvider from "./contexts/AuthProvider.jsx";
+import { store } from './redux/store.js';
+import { createRoot } from 'react-dom/client';
 
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/shop",
-        element: <Shop />,
-      },
-      {
-        path: "shop/:id",
-        element:<SingleProduct />
-      },
-      {
-        path: "/about",
-        element: <About/>
-      },
-      {
-        path: "/signin",
-        element: <Signin/>
-      },
-      {
-        path: "/signup",
-        element: <Signup/>
-      }
-     
-    ],
-  },
-
-  
- 
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider
-  router={router}
-  
-/>
+const Root = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="shop/:id" element={<SingleProduct />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
+
+createRoot(document.getElementById('root')).render(Root);
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -5,12 +5,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import logo from "../assets/images/logo/newlogo.png";
 // import NavItems from '../components/NavItems';
 import AboutUs from '../home/AboutUs';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, siginInFailure } from '../redux/user/userSlice';
 
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -18,11 +20,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if ( !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(siginInFailure('Please fill out all fields.'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -30,15 +31,16 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(siginInFailure(data.message));
       }
-      setLoading(false);
+     
       if(res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(siginInFailure(error.message));
+      
     }
   };
   return (

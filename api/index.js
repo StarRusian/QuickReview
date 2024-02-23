6f3652ee5a8a39a8405d721d9eada682546ee995
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-23 12:31:54 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
 import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 // import userRoutes from "./routes/user.route.js";
 // import  authRoutes from "./routes/auth.route.js";
 
 
 
-// dotenv.config();
+dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 

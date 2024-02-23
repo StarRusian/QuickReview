b46e7ecf27d2593de97012ab7376c8a3bb4f75f2
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-23 12:55:04 +0530

    only user model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import userRoutes from "./routes/user.route.js";
-// import  authRoutes from "./routes/auth.route.js";
+// import userRoutes from './routes/user.route.js';
+// import authRoutes from './routes/auth.route.js';
 
 
 

c286fd6b188ed347626eaffbf3a3f23333496d6f
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-24 01:38:30 +0530

    sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useLocation } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParms = new URLSearchParams(location.search);
+    const tabFormUrl = urlParms.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+      
+    }
+  }, [location.search]);
+
+  return <div>
+    <div className="">
+      {/* Sidebar */}
+
+      <DashSidebar/>
+
+    </div>
+    {/* profile */}
+    {tab == 'profile' && <DashProfile/>}
+  </div>;
 }

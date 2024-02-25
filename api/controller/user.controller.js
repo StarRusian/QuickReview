7dba322b4f511277c703364db0edb9e9f2649374
StarRusian <rohitkumarmukandpur@gmail.com>
2024-02-25 15:27:24 +0530

    add user delete account

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -7,8 +7,7 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log(req.user.id )
-  console.log(req.user.id )
+ 
   if (req.user.id !== req.params.userId) {
     return next(errorhandler(403, 'You are not allowed to update this user'));
   }
@@ -52,6 +51,18 @@ export const updateUser = async (req, res, next) => {
       );
       const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
   } catch (error) {
     next(error);
   }
